7185f35dc37e86639007ca7eab3ed8f3ef6f8e69
Nipu Matabbar <matabbarnipu@gmail.com>
2025-07-14 13:40:15 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded( {extended: true }));
 
 app.get('/', (req,res)=> {
-    res.send("Hello world")
+    res.send("Hello world");
 })
 
 export default app ;
